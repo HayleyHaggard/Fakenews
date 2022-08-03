f90c4f1d19a7133df17d9f381bc393da3c349885
HayleyHaggard <nastena_panova2000@mail.ru>
2022-08-03 16:12:20 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+function Header() {
+  return (
+    <header className="header">
+
+      {/* <div className="container">
+        <div className="header__inner">
+          <a className="logo" >
+            <img className="logo__img" src="img/logo.svg" alt="logo"> </img>
+          </a>
+          <nav className="menu">
+            <button className="menu__btn">
+              <span></span>
+            </button>
+            <ul className="menu__list">
+              <li className="menu__list-item">
+                <a className="menu__list-link"></a>
+              </li>
+              <li className="menu__list-item">
+                <a className="menu__list-link"></a>
+              </li>
+              <li className="menu__list-item">
+                <a className="menu__list-link"></a>
+              </li>
+              <li className="menu__list-item">
+                <a className="menu__list-link"></a>
+              </li>
+              <li className="menu__list-item">
+                <a className="menu__list-link"></a>
+              </li>
+              <li className="menu__list-item">
+                <a className="menu__list-link"></a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div> */}
+
+    </header>
+  );
+}
+
+export default Header;
