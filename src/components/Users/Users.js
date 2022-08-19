3ce370c08a9d8703cf9bc9d21c36ca786f51f918
HayleyHaggard <nastena_panova2000@mail.ru>
2022-08-19 10:40:51 +0300

    fixed buttons follow/unfollow

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,6 +1,8 @@
 import { NavLink } from 'react-router-dom';
 import userImage from '../../assets/images/user.png';
 import './Users.scss'
+import axios from 'axios';
+
 
 let Users = (props) => {
 
@@ -34,8 +36,38 @@ let Users = (props) => {
             </div>
             <div>
               {u.followed
-                ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+                ? <button onClick={() => {
+
+                  axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                    withCredentials: true,
+                    headers: {
+                      "API-KEY": "10f79686-7638-4861-a038-fcca91fc433f"
+                    }
+                  })
+                    .then(response => {
+                      if (response.data.resultCode === 0) {
+                        props.unfollow(u.id)
+                      }
+                    });
+
+
+                }}>Unfollow</button>
+                : <button onClick={() => {
+
+                  axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                    withCredentials: true,
+                    headers: {
+                      "API-KEY": "10f79686-7638-4861-a038-fcca91fc433f"
+                    }
+                  })
+                    .then(response => {
+                      if (response.data.resultCode === 0) {
+                        props.follow(u.id);
+                      }
+                    });
+
+
+                }}>Follow</button>}
             </div>
           </span>
           <span>
